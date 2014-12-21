302e102ff6cd918d88d4c470d6d9870cc0edaa67
Simeon Pashley <simeon@ringalpha.com>
2014-12-21 16:18:38 +0000

    Long outstanding update

---
+++ install_phpcs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo phpcs --config-set tab_width 4
+sudo phpcs --config-set installed_paths  /_sites/Standards_ApprovedFood/ApprovedFood,/_sites/Standards_ApprovedFood/wpcs,/_sites/Standards_ApprovedFood/phpcompat,/_sites/Standards_ApprovedFood/a24,/_sites/Standards_ApprovedFood/phpcs-security-audit
