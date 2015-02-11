455642fa34235caef04d346b79f8a73bae3d8d2f
Simeon Pashley <simeon@ringalpha.com>
2015-02-11 18:44:47 +0000

    expose fix requests in comments
    //@todo
    //@fixme
    //@xxx
    
    will generate errors

---
+++ ApprovedFood/ApprovedFood/Sniffs/Commenting/FixRequestSniff.php
@@ -0,0 +1,20 @@
+<?php
+class ApprovedFood_Sniffs_Commenting_FixRequestSniff implements PHP_CodeSniffer_Sniff {
+    protected $_fixRequests  = array('@todo', '@fixme', '@xxx');
+    
+    public function register()
+    {
+        return array(T_COMMENT);
+    }
+    
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        foreach ($this->_fixRequests as $fixRequest) {
+            if (stripos($tokens[$stackPtr]['content'], $fixRequest)) {
+                $phpcsFile->addError($fixRequest, $stackPtr);
+                continue;
+            }
+        }
+    }
+}
